d17de59ec8a7ab18964e9ac40ea773d5542e1dab
yuhe <gfgkmn@gmail.com>
2021-04-18 09:12:18 +0800

    try eaf, but still a litter slow

--- init.el
+++ init.el
@@ -124,12 +124,16 @@
                       counsel
                       ivy
                       smartparens
+                      iedit
                       expand-region
                       winum
                       ;; for lisp programming
                       s
                       f
-                      iedit
+                      ;; eaf dependency
+                      ctable
+                      epc
+                      deferred
                       ;; roam research
                       org-roam
                       ;; ;; --- Major Mode ---
@@ -550,3 +554,20 @@ INITIAL-INPUT can be given as the initial minibuffer input."
 
 ;; flycheck config
 (add-hook 'prog-mode-hook 'flycheck-mode)
+
+;; ;; eaf config, still a litter slow
+;; (use-package eaf
+;;   :load-path "~/.emacs.d/site-lisp/emacs-application-framework" ; Set to "/usr/share/emacs/site-lisp/eaf" if installed from AUR
+;;   :init
+;;   (use-package epc :defer t :ensure t)
+;;   (use-package ctable :defer t :ensure t)
+;;   (use-package deferred :defer t :ensure t)
+;;   (use-package s :defer t :ensure t)
+;;   :custom
+;;   (eaf-browser-continue-where-left-off t)
+;;   :config
+;;   (eaf-setq eaf-browser-enable-adblocker "true")
+;;   (eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
+;;   (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
+;;   (eaf-bind-key take_photo "p" eaf-camera-keybinding)
+;;   (eaf-bind-key nil "M-q" eaf-browser-keybinding)) ;; unbind, see more in the Wiki
