6d9f4130ab77c821c7f7ce0fb91beb4413f073b6
yuhe <gfgkmn@gmail.com>
2021-05-12 14:47:51 +0800

    delete evil-multiedit, it not seem as vim

--- init.el
+++ init.el
@@ -282,24 +282,6 @@
 (evil-collection-define-key 'normal 'eww-mode-map
   "^" 'evil-first-non-blank)
 
-(use-package evil-multiedit
-  :ensure t)
-
-(define-key evil-visual-state-map "R" 'evil-multiedit-match-all)
-(define-key evil-normal-state-map (kbd "M-d") 'evil-multiedit-match-and-next)
-(define-key evil-visual-state-map (kbd "M-d") 'evil-multiedit-match-and-next)
-(define-key evil-insert-state-map (kbd "M-d") 'evil-multiedit-toggle-marker-here)
-(define-key evil-normal-state-map (kbd "M-D") 'evil-multiedit-match-and-prev)
-(define-key evil-visual-state-map (kbd "M-D") 'evil-multiedit-match-and-prev)
-(define-key evil-visual-state-map (kbd "C-M-D") 'evil-multiedit-restore)
-(define-key evil-multiedit-state-map (kbd "RET") 'evil-multiedit-toggle-or-restrict-region)
-(define-key evil-motion-state-map (kbd "RET") 'evil-multiedit-toggle-or-restrict-region)
-(define-key evil-multiedit-state-map (kbd "C-n") 'evil-multiedit-next)
-(define-key evil-multiedit-state-map (kbd "C-p") 'evil-multiedit-prev)
-(define-key evil-multiedit-insert-state-map (kbd "C-n") 'evil-multiedit-next)
-(define-key evil-multiedit-insert-state-map (kbd "C-p") 'evil-multiedit-prev)
-(evil-ex-define-cmd "ie[dit]" 'evil-multiedit-ex-match)
-
 (global-evil-leader-mode)
 (evil-mode t)
 
